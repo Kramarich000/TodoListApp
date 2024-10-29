94bc8ebf2e5e918f6d35ab0a4cf15b4902324dfb
Karen <ipepela1@mail.ru~>
2024-10-29 21:16:02 +0300

    добавление задач

--- Program.cs
+++ Program.cs
@@ -1,23 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace TodoListApp
+﻿static void AddTask()
 {
-    class Program
-    {
-        static List<string> tasks = new List<string>();
-
-        static void Main(string[] args)
-        {
-            // Вызов метода меню
-        }
-
-        static void AddTask()
-        {
-            Console.Write("Введите описание задачи: ");
-            string taskDescription = Console.ReadLine();
-            tasks.Add(taskDescription);
-            Console.WriteLine("Задача добавлена.");
-        }
-    }
+    Console.Write("Введите описание задачи: ");
+    string taskDescription = Console.ReadLine();
+    tasks.Add(taskDescription);
+    Console.WriteLine("Задача добавлена.");
 }

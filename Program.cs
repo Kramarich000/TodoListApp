9af5ba8b5cb708721edcb6d219ffaef3804b2c37
Karen <ipepela1@mail.ru~>
2024-10-29 21:17:17 +0300

    view

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-
 static void ViewTasks()
 {
     Console.WriteLine("Список задач:");
@@ -12,42 +11,5 @@ static void ViewTasks()
         {
             Console.WriteLine($"{i + 1}. {tasks[i]}");
         }
-
-
-using System;
-using System.Collections.Generic;
-
-namespace TodoListApp
-{
-    class Program
-    {
-        static List<string> tasks = new List<string>();
-
-        static void Main(string[] args)
-        {
-            // Вызов метода меню
-        }
-
-        static void AddTask()
-        {
-            Console.Write("Введите описание задачи: ");
-            string taskDescription = Console.ReadLine();
-            tasks.Add(taskDescription);
-            Console.WriteLine("Задача добавлена.");
-        }
-
-static void RemoveTask()
-{
-    ViewTasks(); // Показать текущие задачи перед удалением
-    Console.Write("Введите номер задачи для удаления: ");
-    if (int.TryParse(Console.ReadLine(), out int taskNumber) && taskNumber > 0 && taskNumber <= tasks.Count)
-    {
-        tasks.RemoveAt(taskNumber - 1);
-        Console.WriteLine("Задача удалена.");
-    }
-    else
-    {
-        Console.WriteLine("Неверный номер задачи.");
-
     }
 }

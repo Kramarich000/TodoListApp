b6175ac1fdd1ce6792ca9c598f582f6fea615ca5
Karen <ipepela1@mail.ru~>
2024-10-29 21:09:43 +0300

    добавлен метод просмотра задач

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void ViewTasks()
+{
+    Console.WriteLine("Список задач:");
+    if (tasks.Count == 0)
+    {
+        Console.WriteLine("Нет задач для отображения.");
+    }
+    else
+    {
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {tasks[i]}");
+        }
+    }
+}

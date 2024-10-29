5aaf7df911fb9ca6c15fdd6e247ff000acbf499b
Karen <ipepela1@mail.ru~>
2024-10-29 21:07:24 +0300

    добавлен метод для добавления задач

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoListApp
+{
+    class Program
+    {
+        static List<string> tasks = new List<string>();
+
+        static void Main(string[] args)
+        {
+            // Вызов метода меню
+        }
+
+        static void AddTask()
+        {
+            Console.Write("Введите описание задачи: ");
+            string taskDescription = Console.ReadLine();
+            tasks.Add(taskDescription);
+            Console.WriteLine("Задача добавлена.");
+        }
+    }
+}

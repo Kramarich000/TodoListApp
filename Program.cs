03a933f6986c6215189dfd4918a087d5d37c16e9
Karen <ipepela1@mail.ru~>
2024-10-29 21:09:12 +0300

    добавлен метод для удаления задач

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void RemoveTask()
+{
+    ViewTasks(); // Показать текущие задачи перед удалением
+    Console.Write("Введите номер задачи для удаления: ");
+    if (int.TryParse(Console.ReadLine(), out int taskNumber) && taskNumber > 0 && taskNumber <= tasks.Count)
+    {
+        tasks.RemoveAt(taskNumber - 1);
+        Console.WriteLine("Задача удалена.");
+    }
+    else
+    {
+        Console.WriteLine("Неверный номер задачи.");
+    }
+}

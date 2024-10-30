10e69c9ac856ffc612ea8dccdf9c79029933517b
Karen <ipepela1@mail.ru~>
2024-10-30 18:34:27 +0300

    f

--- Program.cs
+++ Program.cs
@@ -69,24 +69,6 @@ namespace TodoListApp
                 Console.WriteLine("Неверный номер задачи.");
             }
         }
-
-        static void EditTask()
-        {
-            ViewTasks(); // Показать текущие задачи перед редактированием
-            Console.Write("Введите номер задачи для редактирования: ");
-            if (int.TryParse(Console.ReadLine(), out int taskNumber) && taskNumber > 0 && taskNumber <= tasks.Count)
-            {
-                Console.Write("Введите новое описание задачи: ");
-                string newDescription = Console.ReadLine();
-                tasks[taskNumber - 1] = newDescription;
-                Console.WriteLine("Задача обновлена.");
-            }
-            else
-            {
-                Console.WriteLine("Неверный номер задачи.");
-            }
-        }
-
         static void ViewTasks()
         {
             Console.WriteLine("Список задач:");
